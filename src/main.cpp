9196548d852efe7148c98c07f2ca39c799ace931
Anton de Winter <adewinter@gmail.com>
2021-02-25 10:53:47 -0800

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <SPI.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinM
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
